d3901ee1635b330a2d00c44458d1022e20ecde78
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2022-11-15 18:55:10 +0530

    adding more routes

--- models/product.js
+++ models/product.js
@@ -1,15 +1,37 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
-module.exports = class Product{
-    constructor(t){
-        this.title = t;
-    }
+const p = path.join(path.dirname(require.main.filename), 'data', 'product.json');
+const getProductsFromFile = (cb) => {
 
-    save(){
-        products.push(this);
-    }
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        }
+        else {
+            cb(JSON.parse(fileContent));
+        }
+    });
 
-    static fetchAll(){ //we can directly call this method on this class, no need to instantiate an object
-        return products;
-    }
 }
+module.exports = class Product {
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+
+    save() {
+
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products));
+        });
+
+    }
+
+    static fetchAll(cb) { //we can directly call this method on this class, no need to instantiate an object
+        getProductsFromFile(cb);
+    }
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/products');
 
 
 
-router.use('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+router.get('/products', adminController.getProducts);
 
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
+
 
 
 module.exports = router;
