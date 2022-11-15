1bde546e7da4ad44adba6a3ab4160c918cedfd54
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2022-11-15 15:21:52 +0530

    Adding controllers

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const rootDir = require('../util/path');
-
-const products = []; 
-
-router.use('/add-product',(req, res, next) => {
-    console.log('inside another middleware');
-    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true});
-});
-
-router.post('/add-product', (req, res) => {
-    products.push({title: req.body.title});
-    res.redirect('/');
-});
+const productsController = require('../controllers/products');
 
 
-exports.routes = router;
-exports.products = products;
+
+router.use('/add-product', productsController.getAddProduct);
+
+router.post('/add-product', productsController.postAddProduct);
+
+
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    res.render('shop', {prods: products, pageTitle: 'Shop', path:'/', hasProducts: products.length > 0, activeShop: true, productCSS: true, layout: false});
-}); //middleware, executed for every request
+router.get('/', productsController.getProducts); 
 
 module.exports = router;
