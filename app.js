3c44a8806224d371b98c29decd7384c57069771d
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2022-11-15 12:04:03 +0530

    Adding more styling

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 
 const http = require('http');
 const bodyParser = require('body-parser');
-const routes = require('./routes');
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
