8ebed70a4db5bc12f55315d46924adf611a1b92e
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2022-11-17 17:48:21 +0530

    finishing SQL

--- models/product.js
+++ models/product.js
@@ -1,34 +1,25 @@
-
-
-const Cart = require('./cart')
-const db = require('../util/database')
-
-
-module.exports = class Product {
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequelize.INTEGER, 
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: Sequelize.STRING,
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
+});
 
-    save() {
-
-        return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)', 
-        [this.title, this.price, this.imageUrl, this.description]);
-       
-    }
-
-    static deleteById(id){
-       
-    }
-
-    static fetchAll() { //we can directly call this method on this class, no need to instantiate an object
-       return db.execute('SELECT * FROM products'); //returns a promise
-    }
-
-    static findById(id) {
-        return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-    }
-};
+module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,9 +1,3 @@
-const mysql = require('mysql2');
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node_complete',
-    password: 'sourya95'
-})
-
-module.exports = pool.promise();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node_complete', 'root', 'sourya95', {dialect: 'mysql', host: 'localhost'});
+module.exports = sequelize;
