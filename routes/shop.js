39135a9decb3f19ed0b72ac3778923c8ab53ab5e
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2022-11-18 15:53:02 +0530

    adding mongodb

--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,25 @@
-const express = require('express');
 const path = require('path');
-const router = express.Router();
+
+const express = require('express');
+
 const shopController = require('../controllers/shop');
 
-router.get('/', shopController.getIndex); 
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
 router.get('/products', shopController.getProducts);
+
 router.get('/products/:productId', shopController.getProduct);
+
 router.get('/cart', shopController.getCart);
+
 router.post('/cart', shopController.postCart);
-router.get('/orders', shopController.getOrders);
-router.get('/checkout', shopController.getCheckout);
+
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
 router.post('/create-order', shopController.postOrder);
 
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
--- util/database.js
+++ util/database.js
@@ -1,3 +1,29 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('node_complete', 'root', 'sourya95', {dialect: 'mysql', host: 'localhost'});
-module.exports = sequelize;
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://sourya:tCCF!wSTt9-rY7H@cluster0.gf2zckt.mongodb.net/test'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
