3a9739ba39a3749e3fd7030dbeee4182166dc3a4
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2022-11-19 12:38:33 +0530

    adding cookies

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const user = require('./models/user');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -32,6 +33,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 mongoose.connect('mongodb+srv://sourya:tCCF!wSTt9-rY7H@cluster0.gf2zckt.mongodb.net/shop?retryWrites=true&w=majority')
