fd2425eb9dad422d7cb807fa45b74088b4806369
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2022-11-22 17:26:45 +0530

    adding Stripe payments

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const stripe = require('stripe')(`${STRIPE_SECRET}`);
 const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
@@ -132,7 +133,52 @@ exports.postCartDeleteProduct = (req, res, next) => {
     });
 };
 
-exports.postOrder = (req, res, next) => {
+exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
+  req.user
+  .populate('cart.items.productId')
+  .execPopulate()
+  .then(user => {
+    const products = user.cart.items;
+    let total = 0;
+    products.forEach(p => {
+      total += p.quantity + p.productId.price;
+    })
+    return stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: products.map(p => {
+        return {
+          name: p.productId.title,
+          description: p.productId.description,
+          amount: p.productId.price + 100,
+          currency: 'usd',
+          quantity: p.quantity
+        }
+      }),
+      success_url: req.protocol + '://' + req.get('host') + '/checkout/success', 
+      cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel'
+    })
+    
+  })
+  .then(session => {
+    res.render('shop/checkout', {
+      path: '/checkout',
+      pageTitle: 'Checkout',
+      products: products,
+      totalSum: total,
+      sessionId: session.id
+    })
+  })
+  .catch(err => {
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(error);
+  })
+  
+}
+
+exports.getCheckoutSuccess = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
     .execPopulate()
