2e4ef18c1d65c745a0d999a13e2fa6e1744a1c3d
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2022-11-25 10:34:20 +0530

    heroku deployment

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ const store = new MongoDBStore({
   collection: 'sessions'
 });
 const csrfProtection = csrf();
-const privateKey = fs.readFileSync('server.key');
-const certificate = fs.readFileSync('server.cert');
+//const privateKey = fs.readFileSync('server.key');
+//const certificate = fs.readFileSync('server.cert');
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'images')
